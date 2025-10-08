1e9871de576b294a4fc966c6fcf0bf937fcc31d5
Prince kumar <prince.m2hgamerz@gmail.com>
2025-10-08 19:49:01 +0530

    Update ws.js

--- api/ws.js
+++ api/ws.js
@@ -1,27 +1,45 @@
 export const config = {
-  runtime: "edge",
+  runtime: 'edge',
 };
 
-let rooms = {};
+let rooms = new Map();
 
-export default async (req) => {
+export default async function handler(req) {
   const { searchParams } = new URL(req.url);
-  const roomId = searchParams.get("room");
+  const roomId = searchParams.get('room');
+
+  // Upgrade request to WebSocket
+  if (req.headers.get('upgrade') !== 'websocket') {
+    return new Response('Expected WebSocket', { status: 400 });
+  }
+
   const { socket, response } = Deno.upgradeWebSocket(req);
 
-  if (!rooms[roomId]) rooms[roomId] = [];
-  const peers = rooms[roomId];
+  if (!rooms.has(roomId)) {
+    rooms.set(roomId, []);
+  }
+  const peers = rooms.get(roomId);
 
-  socket.onmessage = (e) => {
-    peers.forEach((peer) => {
-      if (peer !== socket) peer.send(e.data);
-    });
+  socket.onmessage = (event) => {
+    // Broadcast messages to other peers in the room
+    for (const peer of peers) {
+      if (peer !== socket) {
+        try {
+          peer.send(event.data);
+        } catch (err) {
+          console.error('Send error:', err);
+        }
+      }
+    }
   };
 
   socket.onclose = () => {
-    rooms[roomId] = peers.filter((p) => p !== socket);
+    const updated = peers.filter((p) => p !== socket);
+    rooms.set(roomId, updated);
+    console.log(`Socket closed for room ${roomId}`);
   };
 
   peers.push(socket);
+  console.log(`Socket connected to room ${roomId}`);
   return response;
-};
+}
